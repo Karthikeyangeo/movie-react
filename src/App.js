0db37f3e50fce700bb7e5b58f73227dce7854b36
karthikeyangeo <karthikeyangeo@gmail.com>
2021-11-14 19:51:44 +0530

    update on import

--- src/App.js
+++ src/App.js
@@ -8,12 +8,9 @@ import { MovieList , MovieDetails} from './MovieList';
 import { AddColor } from './AddColor';
 import {EditMovie} from './EditMovie';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
 
 
 
