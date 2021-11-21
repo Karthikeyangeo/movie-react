0d1aae2286e8b150ca71580a7245cbedaeabaef4
karthikeyangeo <karthikeyangeo@gmail.com>
2021-11-21 21:37:24 +0530

    formik and yup validations

--- src/BasicForm.js
+++ src/BasicForm.js
@@ -1,53 +1,65 @@
 import {useFormik} from  "formik";
+import * as yup from 'yup';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-const formValidation = (values)=>{
-    const errors ={};
-    console.log("formValidation",values);
-    if(values.email.length < 5){
-        errors.email ="Please enter min 5 characters"
-    }
-    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-      }
-    if(values.password.length < 8){
-        errors.password = "Please enter min 8 characters"
-    }
-    console.log(errors)
-    return errors;
-}
 
+const formValidationSchema = yup.object({
+    email: yup.string()
+                .min(5,"Minimum length of email should be 5 chars")
+                .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, "Invalid Email Address")
+                .required("Mandatory field"),
+    password : yup.string()
+                    .min(8,"Minimum length of password should be 5 chars")
+                    .max(12,"Max length of password should be 12 chars")
+                    .required("Mandatory field"),
+})
 
 export function BasicForm() {
-    const formik = useFormik({
-        initialValues : {email: "guvi",password:""},
-        validate:formValidation,
+    const {handleSubmit,values,handleBlur,handleChange,errors,touched} = useFormik({
+        initialValues : {email: "",password:""},
+        validationSchema:formValidationSchema,
         onSubmit :(values) => {
             console.log("onSubmit", values);
         },
     });
 
     return(
-        <form onSubmit ={formik.handleSubmit}>
-            <input
-                id="email"
-                name="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                type="email"
-                placeholder="Please enter your email"
-            />
+        <form onSubmit ={handleSubmit}>
+            <div className="basic-form">
+                <div className="login-headers">
+                    < img className={"guvi-logo"} alt={"GUVI Logo"} src={"http://surl.li/asyxu"} />  
+                    <h3>Login </h3>
+                </div>
 
-            <input
-                id="password"
-                name="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                type="password"
-                placeholder="Please enter your password"
-            />
-            {formik.errors.password}
-            <button type="submit">Submit</button>
-
+                <TextField
+                    
+                    id="email"
+                    label="Email ID"
+                    variant="outlined"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur = {handleBlur}
+                    type="email"
+                    placeholder="Please enter your email"
+                    helperText = {errors.email && touched.email ? errors.email : ""}
+                />
+                
+                <TextField
+                    id="password"
+                    label="Password"
+                    variant="outlined"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur = {handleBlur}
+                    type="password"
+                    placeholder="Please enter your password"
+                    helperText = {errors.password && touched.password ? errors.password : ""}
+                />
+                
+                <Button variant="contained" color="success" type="submit">Submit</Button>
+            </div>
         </form>
     )
 }
+
